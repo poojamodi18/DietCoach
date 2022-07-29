5a1a76acafeabdef9dc2f12db629edbcea9fcc26
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-07-29 15:32:57 +0530

    Update PaytmConstants.java
    feat: updated paytm constants

--- DietCoach/src/com/utility/PaytmConstants.java
+++ DietCoach/src/com/utility/PaytmConstants.java
@@ -1,8 +1,8 @@
 package com.utility;
 
 public class PaytmConstants {
-	public final static String MID = "oTawxk61478731922897";
-	public final static String MERCHANT_KEY = "H&&N219ZPidO#kqE";
+	public final static String MID = "ngmHbX49594636078903";
+	public final static String MERCHANT_KEY = "nhuvr#6HMFGuA&bq";
 	public final static String INDUSTRY_TYPE_ID = "Retail";
 	public final static String CHANNEL_ID = "WEB";
 	public final static String WEBSITE = "WEBSTAGING";
